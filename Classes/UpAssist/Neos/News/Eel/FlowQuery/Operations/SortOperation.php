755296708cdd5fdc6cf58958bdebe291183c2379
Henjo Hoeksma <henjo@upassist.com>
2022-03-21 17:06:10 +0100

    TASK: Apply migration Neos.Flow-20161124204700
    Adjusts code to package renaming from "TYPO3.Flow" to "Neos.Flow"

--- Classes/UpAssist/Neos/News/Eel/FlowQuery/Operations/SortOperation.php
+++ Classes/UpAssist/Neos/News/Eel/FlowQuery/Operations/SortOperation.php
@@ -6,7 +6,7 @@ namespace UpAssist\Neos\News\Eel\FlowQuery\Operations;
  */
 use TYPO3\Eel\FlowQuery\FlowQueryException;
 use TYPO3\Eel\FlowQuery\Operations\AbstractOperation;
-use TYPO3\Flow\Annotations as Flow;
+use Neos\Flow\Annotations as Flow;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 use TYPO3\Eel\FlowQuery\FlowQuery;
 use TYPO3\TYPO3CR\Domain\Model\Node;
