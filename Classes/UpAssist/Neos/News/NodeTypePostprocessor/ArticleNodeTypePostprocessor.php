ec834f0312ad7601a767998d3492f3fac8018a8a
Henjo Hoeksma <henjo@upassist.com>
2022-03-21 17:06:11 +0100

    TASK: Apply migration Neos.Neos-20161125002322
    Adjusts code to package renaming from "TYPO3.Neos" to "Neos.Neos"

--- Classes/UpAssist/Neos/News/NodeTypePostprocessor/ArticleNodeTypePostprocessor.php
+++ Classes/UpAssist/Neos/News/NodeTypePostprocessor/ArticleNodeTypePostprocessor.php
@@ -2,7 +2,7 @@
 namespace UpAssist\Neos\News\NodeTypePostprocessor;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow package "TYPO3.Neos".            *
+ * This script belongs to the TYPO3 Flow package "Neos.Neos".            *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU General Public License, either version 3 of the   *
